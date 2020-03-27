eb910032f5c0e48f041b1eb51a7ddd1604b01ebc
yannaing-lynn <rhgtbossy@gmail.com>
2020-03-27 03:48:51 -0400

    i

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
+$factory->define(App\Employee::class, function (Faker $faker) {
+    return [
+        'emp_name' => $faker->name,
+        'salary'   => $faker->numberBetween($min = 5000, $max = 90000),
+        'job_description'=> $faker->paragraph
+    ];
+});
